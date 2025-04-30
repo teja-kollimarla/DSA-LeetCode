d23e02e1fc71a38e556e63b0351a71cc00eeb2a7
Naga sai Satya Teja <nagasai.koli@gmail.com>
2025-04-30 07:00:39 +0530

    Time: 3 ms (10.29%) | Memory: 14.4 MB (11.46%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
-    int noOfEven(long long num){
-        int c=0;
-        cout<<num<<endl;
-        while(num>0){
-            c++;
-            num/=10;
-            cout<<"num= "<<num<<endl;
-
-        }
-        return c;
-    }
+    
 
     int findNumbers(vector<int>& nums) {
         int c=0;
         for(auto i :nums){
-            if(noOfEven(i)%2==0){
+            if(to_string(i).length() % 2 ==0){
                 c++;
             }
         }
