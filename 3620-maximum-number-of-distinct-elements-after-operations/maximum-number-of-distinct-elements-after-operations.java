bce61fa7a525dc81eb91fe540ffe67cae738cb29
Naga Sai Satya Teja <nagasai.koli@gmail.com>
2025-10-18 23:57:13 +0530

    Time: 36 ms (24.83%) | Memory: 63.6 MB (5.52%) - LeetSync

---
+++ 3620-maximum-number-of-distinct-elements-after-operations/maximum-number-of-distinct-elements-after-operations.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+
+class Solution {
+
+    public int count(int n,int[] nums){
+        int c=0;
+        for(int i:nums){
+            if(i==n){
+                c++;
+            }
+        }
+        return c;
+    }
+
+    public int uniqueMax(int[] nums){
+        Set<Integer> sc=new HashSet<>();
+        for(int e:nums){
+            sc.add(e);
+        }
+        return sc.size();
+    }
+
+    public int maxDistinctElements(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ls=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
+            int min =nums[i]-k;
+            int max=nums[i]+k;
+            int as=Math.max(min,ls+1);
+            if(as<=max){
+                nums[i]=as;
+                ls=as;
+            }
+
+        }
+        return uniqueMax(nums);
+        
+    }
+}
