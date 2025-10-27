74a585b8910e1376121c432a41aff8ea9f015a69
Naga Sai Satya Teja <nagasai.koli@gmail.com>
2025-10-28 00:26:11 +0530

    Time: 12 ms (53.73%) | Memory: 45.6 MB (8.84%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
@@ -1,28 +1,20 @@
 class Solution {
     public int numberOfBeams(String[] bank) {
-        int t=0;
-        int a=0;
-       
-        for(int i=0;i<bank.length;i++){
-           int c=0;
-           
-            for(int j=0;j<bank[i].length();j++){
-                if(bank[i].charAt(j)=='1'){
+        int t = 0;
+        int a = 0;
+        int c;
+        for (int i = 0; i < bank.length; i++) {
+            c = 0;
+            for (int j = 0; j < bank[i].length(); j++) {
+                if (bank[i].charAt(j) == '1') {
                     c++;
                 }
             }
-
-            if(c==0){
+            if (c == 0) {
                 continue;
             }
-            a+=t*c;
-            
-            t=c;
-            
-            
-            
-
-
+            a += t * c;
+            t = c;
         }
         return a;
     }
