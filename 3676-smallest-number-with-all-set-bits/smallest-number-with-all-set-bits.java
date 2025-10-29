1257eac2bd46c04ee1090b3271351c260a0ab230
Naga Sai Satya Teja <nagasai.koli@gmail.com>
2025-10-29 19:06:53 +0530

    Time: 1 ms (33.25%) | Memory: 41.3 MB (11.91%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.java
@@ -3,10 +3,13 @@ class Solution {
         return Integer.parseInt(x,2)>=n;
     }
     public int smallestNumber(int n) {
-        String b="1";
+        String b=Integer.toBinaryString(n);
+       b= b.replace('0','1');
+        
         while(!isvalid(n,b)){
-            b+="1";
+             b+="1";
         }
+        
         return Integer.parseInt(b,2);
     }
 }
