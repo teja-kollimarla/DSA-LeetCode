389b963cafef0d3aa4efc1728833c1ecf3bc828c
Naga Sai Satya Teja <nagasai.koli@gmail.com>
2026-01-19 01:30:37 +0530

    Time: 7 ms (6.60%) | Memory: 45.1 MB (17.00%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        Arrays.sort(arr);
+        
+        
+        for(int i=0;i<arr.length;i++){
+            int start=0;
+            int end=arr.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int target=arr[i]*2;
+            if(target>arr[mid]){
+                start=mid+1;
+            }
+            else if(target<arr[mid]){
+                end=mid-1;
+            }
+            else{
+                if(mid!=i)  return true;
+                start=mid+1;
+            }
+        }}
+        return false;
+    }
+}
