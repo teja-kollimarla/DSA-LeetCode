92fe2039259f020c26f27410fe12de08240fa3db
Naga Sai Satya Teja <nagasai.koli@gmail.com>
2026-05-23 16:06:49 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (18.08%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
-    bool check(vector<int>& nums) {
-        int dip = 0;
-        for (int i = 0; i < nums.size() - 1; i++) {
-            if (dip > 1) {
+    bool check(vector<int>& nums) 
+    {
+        int dip = nums.back() > nums[0] ;
+        for(int i=1; i<nums.size(); i++)
+            if(nums[i-1] > nums[i] && ++ dip > 1)
                 return false;
-            }
-            if (nums[i] > nums[i + 1]) {
-                dip++;
-            }
-        }
-        if(nums[0]<nums.back()){
-            dip++;
-        }
-        return dip<2;
+        return true;
     }
 };
