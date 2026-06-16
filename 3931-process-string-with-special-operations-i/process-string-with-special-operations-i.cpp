df08d44f262e51e8b180825ebcea15cc6fb50785
Naga Sai Satya Teja <nagasai.koli@gmail.com>
2026-06-16 21:43:19 +0530

    Time: 26 ms (5.68%) | Memory: 39.8 MB (69.70%) - LeetSync

--- 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     string processStr(string s) {
-        string res="";
-        for(int i:s){
-            if(isalpha(i)){
-                res+=i;
+        string res = "";
+        for (int i : s) {
+            if (isalpha(i)) {
+                res += i;
+            } else if (i == '#') {
+                res += res;
+            } else if (i == '*') {
+                if (!res.empty()) {
+                    res.pop_back();
+                }
+            } else if (i == '%') {
+                reverse(res.begin(), res.end());
             }
-            else{
-                if(i=='#'){
-                    res+=res;
-                }
-                if(i=='*'){
-                    if(!res.empty()){
-                        res.pop_back();
-                    }
-                }
-                if(i=='%'){
-                    reverse(res.begin(),res.end());
-                }
-            }
-            cout<<res<<endl;
 
-
-
+            cout << res << endl;
         }
-        cout<<res<<endl;
+        cout << res << endl;
         return res;
     }
 };
