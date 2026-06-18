d1dd916cc5e41655341c9ef04533d843fa15b320
Naga Sai Satya Teja <nagasai.koli@gmail.com>
2026-06-18 22:44:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (74.16%) - LeetSync

--- 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     double angleClock(int hour, int minutes) {
         hour*=30;
-        double m=minutes*5.5;
-        double teta= abs(hour-m);
-        // cout<<teta<<endl<<hour<<endl<<m;
-        if(teta>180 ){
-            return 360-teta;
-        }
-        return teta;
+        double mins=minutes*6;
+        // 360/(12*60) 12 *60 as 360 true for every hour and for every minute it is  time 60  so this is add on hours 0.5
+        double teta=abs(hour + 0.5*minutes - mins);
+        return min(teta,360-teta);
+// h=12 m=30
+// h=12x30 =360
+//mins =mx6 =30x6=180
+//add on = 0.5xm =0.5x30=15
+//teta=> h+add on - mins =360 +15 -180 =180+15 =195 >180 we need to return min retrun 360-teta 360-195 =165  
     }
 };
