b3f21ffb430781a71955ca0a151b41e292a1be6b
Naga Sai Satya Teja <nagasai.koli@gmail.com>
2026-06-19 21:43:00 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (84.96%) - LeetSync

--- 507-perfect-number/perfect-number.py
+++ 507-perfect-number/perfect-number.py
@@ -1,12 +1,3 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-        if num < 2: return False
-        s = 1
-        i = 2
-        while i * i <= num:
-            if num % i == 0:
-                s += i
-                if i * i != num:
-                    s += num // i
-            i += 1
-        return s == num
+        return num in {6, 28, 496, 8128, 33550336}
