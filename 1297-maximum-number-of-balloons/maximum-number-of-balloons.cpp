6fff1c2ebba05b9cc8956e7ce8c6f5691866263d
Naga Sai Satya Teja <nagasai.koli@gmail.com>
2026-06-22 13:02:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (15.37%) - LeetSync

--- 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
+++ 1297-maximum-number-of-balloons/maximum-number-of-balloons.cpp
@@ -2,24 +2,9 @@ class Solution {
 public:
     int maxNumberOfBalloons(string text) {
         unordered_map<char,int> up;
-        up['b']=0;
-        up['a']=0;
-        up['l']=0;
-        up['o']=0;
-        up['n']=0;
-
-        for(int i=0;i<text.length();i++){
-            if(up.contains(text[i])) up[text[i]]++;
+        for(char c:text){
+            up[c]++;
         }
-        up['l']=up['l']/2;
-        up['o']=up['o']/2;
-        // for (auto& [key, value] : up) {
-        //     cout << "Key: " << key << ", Value: " << value << "\n";
-        // }
-        int mini=INT_MAX;
-        for(auto& [key,value] :up){
-            mini=min(mini,value);
-        }
-        return mini;
+        return min({up['b'],up['a'],up['l']/2,up['o']/2,up['n']});
     }
 };
