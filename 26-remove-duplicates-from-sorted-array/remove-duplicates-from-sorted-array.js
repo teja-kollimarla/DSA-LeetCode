0b8830a3d979ee494631b8c6b88e5d23c4b78583
Naga Sai Satya Teja <nagasai.koli@gmail.com>
2026-09-19 00:10:06 +0530

    Time: 0 ms (100.00%) | Memory: 57.2 MB (73.04%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -3,12 +3,16 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-  let i=0
-  for(let j=1 ;j<nums.length;j++){
-    if(nums[i]!=nums[j]){
+    let i =0 
+    for(let j = 1 ; j<nums.length ;j++ ){
+        if(nums[i]!=nums[j]){
+           
+           
         i++
-        nums[i]=nums[j];
+            nums[i]=nums[j]
+           
+            
+        }
     }
-  }  
-  return i+1
+    return i+1;
 };
